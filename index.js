4babadf95806c2e607cefa0a442d9cc577e91d97
Prishan Fernando <prishanf@yahoo.com>
2023-04-06 17:44:26 -0700

    changes for pdf generation

--- index.js
+++ index.js
@@ -25,15 +25,59 @@ app.get("/api", async (req, res) => {
 
   try {
     let browser = await puppeteer.launch(options);
-
     let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
+    await page.goto(`https://muv-nuxt-app.vercel.app/detailview/${recordId}`,{waitUntil: 'networkidle0'}); 
+    const pdf = await page.pdf({
+        format:"LETTER",
+        printBackground:true,
+        margin: {
+        left:10,
+        right:15,
+        top:10,
+        bottom:10
+        }
+    });
+    await browser.close();
+    res.send(await pdf);
   } catch (err) {
     console.error(err);
     return null;
   }
 });
+
+async function handleRequest(recordId:any){
+  let options = {};
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+        args: [...chromeService.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromeService.defaultViewport,
+        executablePath: await chromeService.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+        };
+    }else{
+        options = {headless: true}
+    }
+    console.log(options,)
+    const browser = await puppeteerService.launch(options);
+    const page = await browser.newPage();
+    const config = useRuntimeConfig()
+    const host = config.public.hostUrl;
+    await page.goto(`${host}/detailview/${recordId}`,{waitUntil: 'networkidle0'}); 
+    const pdf = await page.pdf({
+        format:"LETTER",
+        printBackground:true,
+        margin: {
+        left:10,
+        right:15,
+        top:10,
+        bottom:10
+        }
+    });
+    await browser.close();
+    return pdf;
+}
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
